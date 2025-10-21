7ee1cbb988b699a571927fe7b9474f54460cdcd6
Humpz <aljashumphrey@gmail.com>
2025-10-22 05:14:17 +0800

    Worked On Adding Catalog Service API

---
+++ catalog-service/app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index() {
+        return Product::all();
+    }
+
+    public function show($id) {
+        return Product::findOrFail($id);
+    }
+}
